95498ddb53d72e65b24c48e8e6a9df7e4b859621
Elek Ye <elek@ElekdeMacBook-Pro.local>
2022-07-31 15:48:47 -0400

    first commit

---
+++ Desktop/Python Learning/Narcissist Number.py
@@ -0,0 +1,18 @@
+#python learning pratice
+# Narcissist number
+
+
+i = 100; r = 0; s = 0; t = 0
+
+while i < 1000:
+    r = i // 100
+    s = (i - r * 100) // 10
+    t = i - r * 100 - s * 10
+    if i == (r ** 3 + s ** 3 + t ** 3):
+    
+        print ("i = " + str(i))
+
+   
+        
+    i +=1
+
